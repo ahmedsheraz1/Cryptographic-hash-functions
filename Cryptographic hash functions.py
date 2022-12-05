86cc1f18ee4e9b2ea58145a8c24e902499d186da
Ahmed Sheraz <ahmedsheraz1@gmail.com>
2022-12-05 06:18:28 +0500

    Create Cryptographic hash functions.py

---
+++ Cryptographic hash functions.py
@@ -0,0 +1,7 @@
+Python 3.10.2 (v3.10.2:a58ebcc701, Jan 13 2022, 14:50:16) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+import hashlib
+hashlib.algorithms_guaranteed
+{'sha512', 'sha256', 'shake_256', 'sha3_224', 'sha3_256', 'blake2s', 'sha3_512', 'sha1', 'sha224', 'md5', 'blake2b', 'shake_128', 'sha384', 'sha3_384'}
+hashlib.algorithms_available
+{'whirlpool', 'sha256', 'sha3_224', 'sha3_256', 'sha224', 'sha3_384', 'blake2s', 'sm3', 'sha512_224', 'shake_256', 'sha512', 'ripemd160', 'sha3_512', 'md5-sha1', 'sha1', 'md5', 'md4', 'sha384', 'sha512_256', 'blake2b', 'shake_128'}
